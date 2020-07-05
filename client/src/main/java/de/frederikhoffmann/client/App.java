ff56e1f8a22e0e01f0bf88f4f5693c4d41f87469
Frederik Hoffmann <frederik.hoffmann99@gmail.com>
2020-07-05 14:17:40 +0200

    Client Example

---
+++ client/src/main/java/de/frederikhoffmann/client/App.java
@@ -0,0 +1,41 @@
+package de.frederikhoffmann.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public static void main(String[] args) {
+		shortExample();
+		longExample();
+
+	}
+
+	private static void shortExample() {
+		Double pi = ClientBuilder.newClient()
+				.target("http://localhost:8080/web/primitive/pi")
+				.request()
+				.get()
+				.readEntity(Double.class);
+
+		System.out.println(pi);
+	}
+
+	public static void longExample() {
+		Client client = ClientBuilder.newClient();
+
+		WebTarget target = client.target("http://localhost:8080/web/primitive/kaffee");
+
+		Invocation invocation = target.request().buildGet();
+
+		String kaffee = invocation.invoke().readEntity(String.class);
+
+		System.out.println(kaffee);
+
+	}
+
+}
